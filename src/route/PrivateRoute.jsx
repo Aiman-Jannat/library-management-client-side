447e15f39b612bdda270a1164ff449978bb72ad5
Aiman-Jannat <ummeaimanjannat@gmail.com>
2023-11-08 21:34:16 +0600

    private route added

--- src/route/PrivateRoute.jsx
+++ src/route/PrivateRoute.jsx
@@ -2,8 +2,8 @@ import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../provider/AuthProvider";
 
-
-const PrivateRoute = () => {
+import PropTypes from 'prop-types';
+const PrivateRoute = ({children}) => {
     const {userr, loading} = useContext(AuthContext);
     if(loading)
     return  <div className='flex mt-56 justify-center items-center max-h-screen'><span className="loading text-center loading-bars loading-lg"></span></div>;
@@ -16,3 +16,6 @@ const PrivateRoute = () => {
     
 
 export default PrivateRoute;
+PrivateRoute.propTypes = {
+    children:PropTypes.node
+}
