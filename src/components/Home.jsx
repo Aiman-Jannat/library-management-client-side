09071d1b28b4c8feb7be027b59b05d4456ae484e
Aiman-Jannat <ummeaimanjannat@gmail.com>
2023-11-08 22:16:12 +0600

    second part added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "./Banner";
 import { key } from "localforage";
 import ShowCategories from "./ShowCategories";
 import Accordian from "./Accordian";
+import Feed from "./Feed";
 
 
 const Home = () => {
@@ -22,6 +23,9 @@ const Home = () => {
             <h1 className="bg-purple-200 border w-fit p-2 mx-auto mt-10 text-2xl font-extrabold text-center text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-center text-transparent bg-clip-text bg-gradient-to-r to-blue-600 from-pink-400">Some Query About Us</span> </h1>
             <p className="text-center mt-3">Hey User! Here are some common question that may relate with your question about our service</p>
             <Accordian></Accordian>
+            <h1 className="bg-purple-200 mb-10 border w-fit p-2 mx-auto mt-10 text-2xl font-extrabold text-center text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-center text-transparent bg-clip-text bg-gradient-to-r to-blue-600 from-pink-400">Some Users Feedback</span> </h1>
+            
+            <Feed></Feed>
             
         </div>
     );
